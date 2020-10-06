d3ab265790d3d19616de028ff41b359b18af7565
Alexandr Alexeyenok <alexandr.alexeyenok@gmail.com>
2020-10-06 22:40:05 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int current_balance = 2_000_000_000;
+        int remittance = 500_000_000;
+        int finale = current_balance + remittance;
+        System.out.println(finale);
+    }
+}
